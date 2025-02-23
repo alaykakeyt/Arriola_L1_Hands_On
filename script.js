842570610ee0bf4856827dd9ac2224a307fc14c5
alaykakeyt <alaicakyte@gmail.com>
2025-02-23 18:04:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let fName = "Alaica Kyte";
 let lName = "Arriola";
 let course = "BSIT";
 let school = "Camarines Norte State College";
-console.log("My name is " + fName + lName + ".");
+console.log("My name is " + fName + " " +lName + ".");
 console.log("A " + course + " student at " + school + ".");
