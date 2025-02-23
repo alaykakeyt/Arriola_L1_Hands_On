7e9559bba4185d0c6e10001b300c4d90d09a19da
alaykakeyt <alaicakyte@gmail.com>
2025-02-23 17:56:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+let fName = "Alaica Kyte";
+let lName = "Arriola";
+let course = "BSIT";
+let school = "Camarines Norte State College";
+console.log("My name is " + fName + lName + ".");
+console.log("A " + course + " student at " + school + ".");
